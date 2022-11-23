f13d93a9bee62776ff478cf6659b98b2e4f6ecb0
LMXZ1258 <117049746+heartlistener@users.noreply.github.com>
2022-11-23 15:15:07 +0800

    Update script_life.js

--- blog/js/script_life.js
+++ blog/js/script_life.js
@@ -1,7 +1,7 @@
 let images = document.querySelector('.images')
 let image = document.querySelector('.image')
 let n = 11;
-for (let i = 1; i < n; i++) {
+for (let i = n-1; i >= 1; i--) {
     var lis = document.createElement('div')
     lis.innerHTML = `<img class="img" src='images/life/img (` + i + `).png'>`
     images.appendChild(lis)
@@ -10,7 +10,7 @@ let img = document.querySelectorAll('.img')
 for (let i = 0; i < img.length; i++) {
     img[i].addEventListener("click", () => {
         image.style.display = "flex"
-        let index = i + 1
+        let index = n - i - 1;
         image.innerHTML = "<img src='images/life/img (" + index + ").png'>"
         document.body.style.overflow = 'hidden'; //½ûÖ¹Ò³Ãæ»¬¶¯
     })
