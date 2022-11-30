6e13c4b5fd6fbe3ccfebf79eb5cc963d19835bbd
LMXZ1258 <117049746+heartlistener@users.noreply.github.com>
2022-11-30 17:11:17 +0800

    Update script_life.js

--- blog/js/script_life.js
+++ blog/js/script_life.js
@@ -1,6 +1,6 @@
 let images = document.querySelector('.images')
 let image = document.querySelector('.image')
-let n = 13;
+let n = 17;
 for (let i = n-1; i >= 1; i--) {
     var lis = document.createElement('div')
     lis.innerHTML = `<img class="img" src='images/life/img (` + i + `).png'>`
